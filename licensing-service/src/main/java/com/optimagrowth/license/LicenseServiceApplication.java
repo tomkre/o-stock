ce102e2d832bb96d2a6997efeb9fdb3332b06061
tomkre <krejcar.tomas@seznam.cz>
2022-01-03 15:32:40 +0100

    update

--- licensing-service/src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
+++ licensing-service/src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
@@ -5,6 +5,8 @@ import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.DependsOn;
--- organization-service/src/main/java/com/optimagrowth/organization/OrganizationServiceApplication.java
+++ organization-service/src/main/java/com/optimagrowth/organization/OrganizationServiceApplication.java
@@ -2,10 +2,11 @@ package com.optimagrowth.organization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@RefreshScope
 public class OrganizationServiceApplication {
 
 	public static void main(String[] args) {
